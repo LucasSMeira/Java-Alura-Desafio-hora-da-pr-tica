b2f9a3feb51a326313cdcee77fd00c15deeba577
Lucas Meira <lucasmeiraprofessional@gmail.com>
2024-02-19 16:41:31 -0300

    Programa 1

---
+++ Desafio-Hora-da-Pratica-M3/src/App.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite o valor de um número qualquer e te direi se ele é positivo ou negativo!");
+        int num = Integer.parseInt(entrada.nextLine());
+
+        if(num > 0){
+            System.out.println("Número positivo");
+        }
+        else if(num < 0){
+            System.out.println("Número negativo");
+        }
+        else{
+            System.out.println("Número inexistente!");
+        }
+    
+        entrada.close();
+    }
+}
